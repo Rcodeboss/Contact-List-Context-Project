75b2f340f0119007c069a24663b7e7a853cbe175
Rcodeboss <morejonrafa@gmail.com>
2021-10-18 23:00:01 +0000

    Contact list non started

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,11 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
+			loadContacts()
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
